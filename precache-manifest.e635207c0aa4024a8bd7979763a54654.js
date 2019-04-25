1a1709041ba184b8bcdcb28408cba6d3f9dffb30
Eduardo Espino <lalospn@gmail.com>
2019-04-25 10:37:31 -0500

    Updates

---
+++ precache-manifest.e635207c0aa4024a8bd7979763a54654.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e97a115dbb3b14259372dfcfeae12cf",
+    "url": "/cgasociados/index.html"
+  },
+  {
+    "revision": "e1578fed6159c2c2f1f1",
+    "url": "/cgasociados/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "5de908123f71d37b1c11",
+    "url": "/cgasociados/static/css/main.72018ebf.chunk.css"
+  },
+  {
+    "revision": "e1578fed6159c2c2f1f1",
+    "url": "/cgasociados/static/js/2.4a061337.chunk.js"
+  },
+  {
+    "revision": "5de908123f71d37b1c11",
+    "url": "/cgasociados/static/js/main.b26be5f8.chunk.js"
+  },
+  {
+    "revision": "a3af3302c6f8ce1fdd1d",
+    "url": "/cgasociados/static/js/runtime~main.41632ecd.js"
+  },
+  {
+    "revision": "a898e5d169f6e6ef6aa9076a971d9891",
+    "url": "/cgasociados/static/media/bug.a898e5d1.svg"
+  },
+  {
+    "revision": "4f350faaa2a13ad1f95c62a01c9934ad",
+    "url": "/cgasociados/static/media/cga.4f350faa.svg"
+  }
+]);
